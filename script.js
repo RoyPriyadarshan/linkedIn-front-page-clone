e4156d00ef3db4f2d061a71f29a6112cf917deea
Priyadarshan <skullboydanger1@gmail.com>
2020-07-03 23:42:22 +0530

    Done

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function myFunction() {
       }
     }
   } 
+
+  /*This code makes navbar sticky*/
+
+  // window.onscroll = function() {
+  //   myFunction()
+  // };
+  // var navbar = document.getElementById("navbar");
+  //   var sticky = navbar.offsetTop;
+  
+  // function myFunction() {
+  //   if (window.pageYOffset >= sticky) {
+  //     navbar.classList.add("sticky")
+  //   } 
+  //   else if(window.pageYOffset < sticky) {
+  //     navbar.classList.remove("sticky");
+  //   }
+  // } 
